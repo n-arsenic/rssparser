f816143e7c94f6d4a2c68cd38e74835354c335f1
php_deva <macpro@MacBook-Pro-Mac.local>
2019-03-28 16:37:01 +0300

    fix xml decode, xml parse page

--- src/rssnews/crawler/asapWorker.go
+++ src/rssnews/crawler/asapWorker.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	//	"encoding/xml"
 	"bytes"
+	"encoding/xml"
 	"fmt"
 	"github.com/lib/pq"
 	"golang.org/x/net/context"
@@ -13,7 +13,7 @@ import (
 	"log"
 	"net"
 	"net/http"
-	"regexp"
+	//	"regexp"
 	pbf "rssnews/protonotify"
 	"rssnews/services/scheduler"
 	"time"
@@ -21,7 +21,7 @@ import (
 
 const (
 	port       = ":50051"
-	MAX_MEMORY = 1 * 1024 //max requested content length
+	MAX_MEMORY = 1024 * 1024 //max requested content length
 )
 
 type (
@@ -73,12 +73,10 @@ func main() {
 //[TODO] allocate this to another pkg
 func (wr *Worker) Work(ch chan scheduler.Service) {
 	fmt.Println("init work")
-	//	fmt.Printf("v\n", charmap.All)
 	for {
 		//condition for close channel
 		schedule := <-ch
 
-		var content string
 		resp, err := http.Get(schedule.Rss_url)
 		if err != nil {
 			fmt.Println(err)
@@ -90,42 +88,54 @@ func (wr *Worker) Work(ch chan scheduler.Service) {
 
 		defer resp.Body.Close()
 
-		limReader := io.LimitReader(resp.Body, MAX_MEMORY)
-		buff := bytes.NewBuffer([]byte{})
-		_, ierr := io.Copy(buff, limReader)
-		content = buff.String()
-
-		fmt.Println(ierr, content)
-
-		reg, _ := regexp.Compile(`xml[\s]+version.+encoding="([a-zA-Z0-9\-:]+)"`)
-		match := reg.FindStringSubmatch(content)
-
-		if len(match) > 1 {
-			charset := match[1]
-			enc, err := htmlindex.Get(charset)
-
-			fmt.Println(err)
-
-			bufReader := bytes.NewReader(buff.Bytes())
-			encReader := enc.NewDecoder().Reader(bufReader)
-			buf := &bytes.Buffer{}
-			_, ioerr := io.Copy(buf, encReader)
-			content = buf.String()
-
-			fmt.Println(ioerr, content)
-		}
+		wr.XMLParser(resp.Body)
 
 		//is it success to write content? yes - update plan_start
 	}
 }
 
-/*
-func (wr *Worker) HtmlParser(content []byte) {
-	var data struct {
-		Channel []Channel `xml:"channel"`
-	}
+func (wr *Worker) XMLParser(rbody io.Reader) {
 
-	if err := xml.Unmarshal([]byte(content), &data); err != nil {
+	type (
+		Rss struct {
+			Channel struct {
+				Title       string `xml:"title"`
+				Link        string `xml:"link`
+				Description string `xml:"description"`
+				PubDate     string `xml:"pubDate"`
+				Item        []struct {
+					Title       string `xml:"title"`
+					Link        string `xml:"link`
+					Description string `xml:"description"`
+					Author      string `xml:"author"`
+					Category    string `xml:"category"`
+					PubDate     string `xml:"pubDate"`
+				} `xml:"item"`
+			} `xml:"channel"`
+		}
+	)
+
+	result := &Rss{}
+
+	limReader := io.LimitReader(rbody, MAX_MEMORY)
+	buff := bytes.NewBuffer([]byte{})
+	_, ierr := io.Copy(buff, limReader)
+
+	xdec := xml.NewDecoder(bytes.NewReader(buff.Bytes()))
+	xdec.CharsetReader = identReader
+
+	fmt.Println(ierr)
+
+	if err := xdec.Decode(result); err != nil {
 		log.Fatal(err)
 	}
-}*/
+
+	fmt.Println("Result", result)
+}
+
+func identReader(encoding string, input io.Reader) (io.Reader, error) {
+	enc, err := htmlindex.Get(encoding)
+	encReader := enc.NewDecoder().Reader(input)
+	fmt.Println("ident err", err)
+	return encReader, err
+}
